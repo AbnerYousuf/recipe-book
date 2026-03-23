949be6710dc5f1809356935db5aaac7283f3773c
AbnerYousuf <yousufabner@gmail.com>
2026-03-22 21:09:37 -0600

    added recipe view and updated router config

---
+++ src/views/RecipeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <main>
+        recipe {{ $route.params.id }}
+    </main>
+</template>
+
+<script setup lang="ts">
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+</script>
