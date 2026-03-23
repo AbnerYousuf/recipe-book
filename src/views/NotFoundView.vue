fd29f8683b70d2d72babbf3ba73e4d460013c422
AbnerYousuf <yousufabner@gmail.com>
2026-03-22 21:47:00 -0600

    added an error view/error landing

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>404</h1>
+        <p>Page not found</p>
+        <RouterLink :to="{ name: 'home' }">Go Home</RouterLink>
+    </div>
+</template>
+
+<script lang="ts">
+</script>
