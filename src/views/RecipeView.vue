e84f9e2e3e9c578ec7b54d221f0940b06b9ad565
AbnerYousuf <yousufabner@gmail.com>
2026-03-22 23:29:50 -0600

    implemented recipe id watcher so that we can change data (it's necessary because vue uses the same component so fetching data through onmounted doesnt work)

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -6,6 +6,12 @@
 
 <script setup lang="ts">
     import { useRoute } from 'vue-router';
+    import { watch } from 'vue';
 
     const route = useRoute();
+
+    watch(() => route.params.id, () => {
+        console.log('Route ID changed');
+    }, { immediate: true }
+    );
 </script>
