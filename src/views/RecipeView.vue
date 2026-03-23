240a4981e42999dd64cddeb3bc4ee5169702cb4b
AbnerYousuf <yousufabner@gmail.com>
2026-03-23 15:38:29 -0600

    implemented adding new recipes + adding new recipes takes you to the new recipe

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,17 +1,19 @@
 <template>
-    <main>
-        recipe {{ $route.params.id }}
-    </main>
+    <div>
+        <h1>{{ recipe?.name }}</h1>
+        <p>Ingredients: {{ recipe?.ingredients.join(', ') }}</p>
+        <p>Instructions: {{ recipe?.instructions }}</p>
+    </div>
 </template>
 
 <script setup lang="ts">
     import { useRoute } from 'vue-router';
-    import { watch } from 'vue';
+    import { computed } from 'vue';
+    import { useRecipeStore } from '@/stores/recipe';
 
     const route = useRoute();
+    const recipeStore = useRecipeStore();
 
-    watch(() => route.params.id, () => {
-        console.log('Route ID changed');
-    }, { immediate: true }
-    );
+    const recipeId = computed(() => route.params.id as string);
+    const recipe = computed(() => recipeStore.getRecipeById(recipeId.value));
 </script>
